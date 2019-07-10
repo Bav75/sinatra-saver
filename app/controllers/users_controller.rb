2763f24b2bc6e4361ae6da4147570b9ad5330b76
Brian Velez <bvez94@gmail.com>
2019-07-10 12:47:12 -0700

    mocked login routes and start of users/:id route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,22 +18,61 @@ class UsersController < ApplicationController
         #post data validity / checking 
 
         #if data valid then create & save 
-        @user = User.create({
-            name: name,
-            email: email,
-            password: pass
-        })
+            @user = User.create({
+                name: name,
+                email: email,
+                password: pass
+            })
         #else
-        #return to signup page & prompt for
-        #valid data entry 
+            #return to signup page & prompt for
+            #valid data entry 
 
-        #once created, redirect to login page 
+            #once created, redirect to login page 
+    end
+
+    get '/login' do
+
+        #if not already logged in
+            #display login page 
+        #else redirect to logged_in users
+        #page 
+            
+
+    end
+
+    post '/login' do
+
+        #receive login credentials
+
+        email = params[:email]
+        password = params[:password]
+
+        @user = User.find_by(email: email)
+
+        # if @user && @user.authenticate(password)
+        #     session[:user_id] = @user.id
+
+        #if login credentials validated
+                #log user in by adding id 
+                #to session hash 
+
+                #redirect to users homepage 
+                #upon successful login
+            #else
+                
+
     end
 
     
 
     get '/users' do
-        "Here are my users" 
+        "Here are all of my users" 
+    end
+
+    get '/users/:user_id' do
+
+        #show the users homepage
+
     end
 
 end
